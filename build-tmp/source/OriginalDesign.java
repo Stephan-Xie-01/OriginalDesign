31841d04ebe4c128ea82527270652931e43405f2
unknown <STXIE1@697-334-Dale.sf.k12.internal>
2015-08-31 09:21:15 -0700

    Original Design

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -1,4 +1,20 @@
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
+public void setup()
 {
 	size(800, 600);
 	frameRate(60);
@@ -20,7 +36,7 @@ float carElevation = 0;
 
 int drop = 400;
 
-void draw()
+public void draw()
 {
 	background(0, 0, 0);
 	car();
@@ -30,7 +46,7 @@ void draw()
 
 	//turning pt
 	if(carX > 200){
-		carElevation = carElevation - 0.1;
+		carElevation = carElevation - 0.1f;
 	}
 
 	//reset
@@ -44,7 +60,7 @@ void draw()
 	//changes
 	carX = carX + 10;
 	carY = carY + carElevation;
-	carElevation = carElevation*1.02;
+	carElevation = carElevation*1.02f;
 	//System.out.println(carY);
 	//System.out.println(carElevation);
 	drop++;
@@ -55,7 +71,7 @@ void draw()
 
 
 
-void car(){
+public void car(){
 	noStroke();
 	fill(250, 250, 250);
 	
@@ -64,14 +80,14 @@ void car(){
 }
 
 
-void drops(){
+public void drops(){
 	ellipse(carX+10, drop, 30, 10);
 	ellipse(carX+60, drop + 20, 30, 10);
 
 }
 
 int explode = 0;
-void explode(){
+public void explode(){
 
 	if(carX > 900){
 		explode += 50;
@@ -92,3 +108,12 @@ void explode(){
 
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
